ca0865bf335407fd00ece64e2d5a4efa91a20316
songguo <jingminliu@sina.cn>
2016-01-04 20:16:17 +0800

    add packages/requests

--- py_test/django_test4/learn/views.py
+++ py_test/django_test4/learn/views.py
@@ -1,5 +1,8 @@
+#-*- coding:utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "learn/home.html")
+    info_dict = {"site":u"自强学堂", "content":u"各种IT技术教程"}
+    test_list = map(str, range(10))
+    return render(request, "learn/home.html", {"info_dict":info_dict, "tst_list":test_list})
